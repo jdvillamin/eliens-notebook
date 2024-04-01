72af83fa932f7b48a2885e24ba134977451fcaa9
JNIDV <jannealvillamin@gmail.com>
2024-04-01 15:03:08 +0800

    feat: Finished strings

--- cpp/strings/suffix_array.cpp
+++ cpp/strings/suffix_array.cpp
@@ -1,59 +1,48 @@
-vector<ll> sort_cyclic_shifts(string const& s) {
-  ll n = s.size();
-  const ll alphabet = 256;
-  vector<ll> p(n), c(n), cnt(max(alphabet, n), 0);
-  for (ll i = 0; i < n; i++) {
-    cnt[s[i]]++;
-  }
-  for (ll i = 1; i < alphabet; i++) {
-    cnt[i] += cnt[i - 1];
-  }
-  for (ll i = 0; i < n; i++) {
-    p[--cnt[s[i]]] = i;
-  }
+vector<int> sort_cyclic_shifts(string const& s) {
+  int n = s.size();
+  const int alphabet = 256;    
+  vector<int> p(n), c(n), cnt(max(alphabet, n), 0);
+  for (int i = 0; i < n; i++) cnt[s[i]]++;
+  for (int i = 1; i < alphabet; i++) cnt[i] += cnt[i - 1];
+  for (int i = 0; i < n; i++) p[--cnt[s[i]]] = i;
   c[p[0]] = 0;
-  ll classes = 1;
-  for (ll i = 1; i < n; i++) {
-    if (s[p[i]] != s[p[i - 1]]) {
-      classes++;
-    }
+  int classes = 1;
+  for (int i = 1; i < n; i++) {
+    if (s[p[i]] != s[p[i-1]]) classes++;
     c[p[i]] = classes - 1;
   }
-  vector<ll> pn(n), cn(n);
-  for (ll h = 0; (1 << h) < n; ++h) {
-    for (ll i = 0; i < n; i++) {
+  vector<int> pn(n), cn(n);
+  for (int h = 0; (1 << h) < n; ++h) {
+    for (int i = 0; i < n; i++) {
       pn[i] = p[i] - (1 << h);
-      if (pn[i] < 0) {
+      if (pn[i] < 0)
         pn[i] += n;
-      }
     }
     fill(cnt.begin(), cnt.begin() + classes, 0);
-    for (ll i = 0; i < n; i++) {
-      cnt[c[pn[i]]]++;
-    }
-    for (ll i = 1; i < classes; i++) {
-      cnt[i] += cnt[i - 1];
-    }
-    for (ll i = n-1; i >= 0; i--) {
-      p[--cnt[c[pn[i]]]] = pn[i];
-    }
+    for (int i = 0; i < n; i++) cnt[c[pn[i]]]++;
+    for (int i = 1; i < classes; i++) cnt[i] += cnt[i - 1];
+    for (int i = n-1; i >= 0; i--) p[--cnt[c[pn[i]]]] = pn[i];
     cn[p[0]] = 0;
     classes = 1;
-    for (ll i = 1; i < n; i++) {
-      pair<ll, ll> cur = {c[p[i]], c[(p[i] + (1 << h)) % n]};
-      pair<ll, ll> prev = {c[p[i - 1]], c[(p[i - 1] + (1 << h)) % n]};
-      if (cur != prev) {
-        ++classes;
-      }
+    for (int i = 1; i < n; i++) {
+      pair<int, int> cur = {c[p[i]], c[(p[i] + (1 << h)) % n]};
+      pair<int, int> prev = {c[p[i-1]], c[(p[i-1] + (1 << h)) % n]};
+      if (cur != prev) ++classes;
       cn[p[i]] = classes - 1;
     }
     c.swap(cn);
   }
   return p;
 }
-vector<ll> build_suff_arr(string s) {
-  s += (char) 0;
-  vector<ll> sorted_shifts = sort_cyclic_shifts(s);
+vector<int> build_suff_arr(string s) {
+  s += "$";
+  vector<int> sorted_shifts = sort_cyclic_shifts(s);
   sorted_shifts.erase(sorted_shifts.begin());
   return sorted_shifts;
+}
+// compare two substrings
+int compare(int i, int j, int l, int k) {
+  pair<int, int> a = {c[k][i], c[k][(i + l - (1 << k)) % n]};
+  pair<int, int> b = {c[k][j], c[k][(j + l - (1 << k)) % n]};
+  return a == b ? 0 : a < b ? -1 : 1;
 }
--- cpp/strings/z_function.cpp
+++ cpp/strings/z_function.cpp
@@ -1,15 +1,12 @@
-vector<ll> z_function(string s) {
-  ll n = (ll) s.length();
-  vector<ll> z(n);
-  for (ll i = 1, l = 0, r = 0; i < n; ++i) {
-    if (i <= r) {
-      z[i] = min (r - i + 1, z[i - l]);
-    }
-    while (i + z[i] < n && s[z[i]] == s[i + z[i]]) {
-      ++z[i];
-    }
-    if (i + z[i] - 1 > r) {
-      l = i, r = i + z[i] - 1;
+vector<int> z_function(string s) {
+  int n = s.size();
+  vector<int> z(n);
+  for (int i = 1, l = 0, r = 0; i < n; i++) {
+    if (i < r) z[i] = min(r - i, z[i - l]);
+    while (i + z[i] < n && s[z[i]] == s[i + z[i]]) z[i]++;
+    if (i + z[i] > r) {
+      l = i;
+      r = i + z[i];
     }
   }
   return z;
