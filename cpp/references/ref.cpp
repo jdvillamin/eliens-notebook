e1f05dbfecd53ca65e9952a32ce7ba57268a778f
JNIDV <jannealvillamin@gmail.com>
2024-04-01 22:26:59 +0800

    feat: Updated ref

--- cpp/references/ref.cpp
+++ cpp/references/ref.cpp
@@ -1,46 +1,119 @@
-st.insert(4);
-st.erase(4);
-st.empty();
+// vector 
+push_back()
+pop_back()
+size()
+clear()
+erase()
+empty()
+Iterator lower_bound(Iterator first, Iterator last, const val)
+Iterator upper_bound(Iterator first, Iterator last, const val)
+// stack
+push()
+pop()
+top()
+empty()
+size()
+// queue
+push()
+pop()
+front()
+empty()
+back()
+size()
+// priority_queue
+push()
+pop()
+size()
+empty()
+top()
+// set
+insert()
+begin()
+end()
+size()
+find()
+count()
+empty()
+// multiset
+begin()
+end()
+size()
+max_size()
+empty()
+insert(x) // O(log n)
+clear()
+erase(x)
+// map
+begin()
+end()
+size()
+max_size()
+empty()
+pair insert(keyvalue, mapvalue)
+erase(iterator position)
+erase(const g)
+clear()
+// ordered_set
+find_by_order(k)
+order_of_key(k)
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+ 
+#define ordered_set                              \
+    tree<int, null_type, less<int>, rb_tree_tag, \
+         tree_order_statistics_node_update>
+// tuple
+get<i>(tuple)
+make_tuple(a1, a2, ...)
+tuple_size<decltype(tuple)>::value
+tuple1.swap(tuple2)
+tie(a1, a2, ...) = tuple
+tuple_cat(tuple1, tuple2)
+// iterator
+for (auto it = s.begin(); it != s.end(); it++) cout << *it << "\n";
+begin()
+end()
+advance(ptr, k)
+next(ptr, k)
+prev(ptr, k)
 // permutations
-do {
-for (int num : nums) {
-cout << num << " ";
-}
-cout << endl;
-} while (next_permutation(nums.begin(), nums.end()));
+do {} while (next_permutation(nums.begin(), nums.end()));
 // bitset
 int num = 27; // Binary representation: 11011
 bitset<10> s(string("0010011010")); // from right to left
 bitset<sizeof(int) * 8> bits(num);
 int setBits = bits.count();
+bits.set(index, val);
+bits.reset();
+bits.flip();
+bits.all();
+bits.any();
+bits.none();
+bits.test();
+to_string();
+to_ulong();
+to_ullong();
+[], &, |, !, >>=, <<=, &=, |=, ^=, ~;
 // sort
 sort(v.begin(),v.end());
 sort(v.rbegin(),v.rend());
 // custom sort
 bool comp(string a, string b) {
-if (a.size() != b.size()) return a.size() < b.size();
-return a < b;
-}
+  if (a.size() != b.size()) return a.size() < b.size(); 
+  return a < b; }
 sort(v.begin(), v.end(), comp);
-// binary search
-int a = 0, b = n-1;
-while (a <= b) { int k = (a+b)/2; if (array[k] == x) {
-// x found at index k
-} if (array[k] > x) b = k-1; else a = k+1;}
-// iterator
-for (auto it = s.begin(); it != s.end(); it++) {
-cout << *it << "\n";
-}
 // hamming distance
-int hamming(int a, int b) {
-return __builtin_popcount(a^b);
-}
+int hamming(int a, int b) { return __builtin_popcount(a ^ b); }
 // custom comparator for pq
 class Compare {
 public:
-bool operator()(T a, T b){
+bool operator() (T a, T b) {
 if(cond) return true; // do not swap
-return false;
-}
-};
+return false; } };
 priority_queue<PII, vector<PII>, Compare> ds;
+// gcc compiler
+__builtin_popcount(x)
+__builtin_parity(x)
+__builtin_clz(x) // leading
+__builtin_ctz(x) // trailing
