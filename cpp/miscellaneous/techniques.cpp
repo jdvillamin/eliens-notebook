ab3d43dacd69432b1a8c4ffe00424fb1ee389bfc
JNIDV <jannealvillamin@gmail.com>
2024-04-01 22:51:53 +0800

    feat: Added techniques list

---
+++ cpp/miscellaneous/techniques.cpp
@@ -0,0 +1,28 @@
+/*
+Dynamic Programming
+- Bitmask
+- Range
+- Digit
+- Knapsack
+Graph Theory
+- Tree diameter
+- Reversing edges
+- Tree re-rooting
+- DP on trees
+- DFS tree
+- Euler tour
+- Binary Jumping
+- Centroid
+- DAG
+- Condense
+Data Structures
+- Multiple information
+- Binary searching on the tree
+- 2D range query
+- SQRT decomposition
+- Small-to-large
+Sorting and searching
+- Sliding window
+- Two pointers
+- Binary search on the answer
+*/
